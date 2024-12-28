349db37db159274f11e7ca68c41e63e11ca546e2
piero casas <pcasas@newport.com.pe>
2024-12-27 22:30:13 -0500

    add custom hook form in register page

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -4,47 +4,72 @@ import { Button } from '@mui/material';
 import { AuthLayout } from '../layouts/AuthLayout';
 
 export const RegisterPage = () => {
+
+  const { name, email, password, onInputChange } = useForm({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({
+      name,
+      email,
+      password
+    });
+  }
+
   return (
     <AuthLayout title='Register' helperLink={{ text: '¿Ya tienes cuenta? Ingresar', link: '/auth/login' }}>
-      <Grid
-        container
-        direction={'column'}
-        spacing={2}
-        sx={{ mt: 2 }}
-      >
-        <TextField
-          required
-          label='Name'
-          type='text'
-          placeholder='Nombre'
-          name='name'
-        />
-        <TextField
-          required
-          label='Email'
-          type='email'
-          placeholder='correo@gmail.com'
-          name='email'
-        />
-        <TextField
-          required
-          label='Password'
-          type='password'
-          placeholder='Contraseña'
-          name='password'
-        />
-      </Grid>
-      <Grid
-        container
-        spacing={2}
-        sx={{ mt: 2 }}
-      >
-        <Grid size={12}>
-          <Button variant='contained' fullWidth>
-              Login
-          </Button>
+      <form onSubmit={onSubmit}>
+        <Grid
+          container
+          direction={'column'}
+          spacing={2}
+          sx={{ mt: 2 }}
+        >
+          <TextField
+            required
+            label='Name'
+            type='text'
+            placeholder='Nombre'
+            name='name'
+            value={name}
+            onChange={onInputChange}
+          />
+          <TextField
+            required
+            label='Email'
+            type='email'
+            placeholder='correo@gmail.com'
+            name='email'
+            value={email}
+            onChange={onInputChange}
+          />
+          <TextField
+            required
+            label='Password'
+            type='password'
+            placeholder='Contraseña'
+            name='password'
+            value={password}
+            onChange={onInputChange}
+          />
         </Grid>
-      </Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{ mt: 2 }}
+        >
+          <Grid size={12}>
+            <Button variant='contained' fullWidth type='submit'>
+                Login
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      
     </AuthLayout>
   )
 }
